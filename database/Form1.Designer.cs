a7cc8295e93880a235d9a64e20f016b01d43b85f
User <bbv1052@gmail.com>
2020-04-17 10:34:11 +0300

    Добавьте файлы проекта.

---
+++ database/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace database
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.testdbDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.testdbDataSet = new database.testdbDataSet();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameNDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.surNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dateBerthDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sexDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.peoplesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.testdbDataSet1 = new database.testdbDataSet1();
+            this.peoplesTableAdapter = new database.testdbDataSet1TableAdapters.PeoplesTableAdapter();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.bindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.testdbDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testdbDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.peoplesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testdbDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // testdbDataSetBindingSource
+            // 
+            this.testdbDataSetBindingSource.DataSource = this.testdbDataSet;
+            this.testdbDataSetBindingSource.Position = 0;
+            // 
+            // testdbDataSet
+            // 
+            this.testdbDataSet.DataSetName = "testdbDataSet";
+            this.testdbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(604, 50);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(8, 8);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameNDataGridViewTextBoxColumn,
+            this.surNameDataGridViewTextBoxColumn,
+            this.dateBerthDataGridViewTextBoxColumn,
+            this.sexDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.peoplesBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1003, 137);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // nameNDataGridViewTextBoxColumn
+            // 
+            this.nameNDataGridViewTextBoxColumn.DataPropertyName = "NameN";
+            this.nameNDataGridViewTextBoxColumn.HeaderText = "NameN";
+            this.nameNDataGridViewTextBoxColumn.Name = "nameNDataGridViewTextBoxColumn";
+            // 
+            // surNameDataGridViewTextBoxColumn
+            // 
+            this.surNameDataGridViewTextBoxColumn.DataPropertyName = "SurName";
+            this.surNameDataGridViewTextBoxColumn.HeaderText = "SurName";
+            this.surNameDataGridViewTextBoxColumn.Name = "surNameDataGridViewTextBoxColumn";
+            // 
+            // dateBerthDataGridViewTextBoxColumn
+            // 
+            this.dateBerthDataGridViewTextBoxColumn.DataPropertyName = "DateBerth";
+            this.dateBerthDataGridViewTextBoxColumn.HeaderText = "DateBerth";
+            this.dateBerthDataGridViewTextBoxColumn.Name = "dateBerthDataGridViewTextBoxColumn";
+            // 
+            // sexDataGridViewTextBoxColumn
+            // 
+            this.sexDataGridViewTextBoxColumn.DataPropertyName = "Sex";
+            this.sexDataGridViewTextBoxColumn.HeaderText = "Sex";
+            this.sexDataGridViewTextBoxColumn.Name = "sexDataGridViewTextBoxColumn";
+            // 
+            // peoplesBindingSource
+            // 
+            this.peoplesBindingSource.DataMember = "Peoples";
+            this.peoplesBindingSource.DataSource = this.testdbDataSet1;
+            // 
+            // testdbDataSet1
+            // 
+            this.testdbDataSet1.DataSetName = "testdbDataSet1";
+            this.testdbDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // peoplesTableAdapter
+            // 
+            this.peoplesTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1126, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.testdbDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testdbDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.peoplesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testdbDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource testdbDataSetBindingSource;
+        private testdbDataSet testdbDataSet;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private testdbDataSet1 testdbDataSet1;
+        private System.Windows.Forms.BindingSource peoplesBindingSource;
+        private testdbDataSet1TableAdapters.PeoplesTableAdapter peoplesTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameNDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn surNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dateBerthDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sexDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.BindingSource bindingSource2;
+    }
+}
+
---
+++ database/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+namespace database
+{
+    public partial class Form1 : Form
+    {
+        public static string conStr = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\\testdb.mdb;Persist S" +
+           "ecurity Info=True;Jet OLEDB:Database Password=12345";
+        private OleDbConnection conn;
+        private BindingSource bindingSource = new BindingSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "testdbDataSet1.Peoples". При необходимости она может быть перемещена или удалена.
+            //this.peoplesTableAdapter.Fill(this.testdbDataSet1.Peoples);
+
+            conn = new OleDbConnection(conStr);
+            conn.Open();
+
+            //var command = conn.CreateCommand();
+           // command.CommandText = "INSERT INTO Peoples (id, NameN, SurName, DateBerth, Sex)" + " Values ('0', 'Bogdan', 'Bieliaiev', '20', 'M')";
+
+            // OleDbCommand command = new OleDbCommand("INSERT INTO Peoples (NameN, SurName, DateBerth, Sex)" + " Values ('Bohdan', 'Bieliaev', '20', 'M')");
+            OleDbCommand command = new OleDbCommand("SELECT * FROM Peoples");
+            command.Connection = conn;
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(command);
+            DataSet dataset = new DataSet();
+
+            adapter.Fill(dataset);
+            //conn.Close();
+            dataGridView1.AutoGenerateColumns = true;
+            bindingSource.DataSource = dataset.Tables[0];
+            dataGridView1.DataSource = bindingSource;
+            MessageBox.Show("Load");
+          
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            conn.Close();
+        }
+    }
+}
